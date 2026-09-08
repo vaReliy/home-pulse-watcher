f02eb48a9cd32911d06d0cb5bb5e3f010cbdb776
vaReliy <valeriy@i.ua>
2026-09-08 16:27:40 +0300

    fix(config): released firmware version 3.5.4 STABLE

--- firmware/esp32c3/src/config.h
+++ firmware/esp32c3/src/config.h
@@ -9,7 +9,7 @@
  */
 
 // Firmware Version (reported to backend on every status ping)
-#define FIRMWARE_VERSION "3.5.4-beta.1"
+#define FIRMWARE_VERSION "3.5.4"
 
 // GPIO Configuration
 #define POWER_SENSE_PIN 2       // GPIO for power detection (connect to optocoupler output)
